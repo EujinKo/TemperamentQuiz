a6c7db73dc10b202ade95df8001bb958338dcac7
EujinKo <42593556+EujinKo@users.noreply.github.com>
2019-09-20 02:23:25 -0700

    Need to change sharedPref using putextra intent

--- app/src/main/java/com/example/temperamentquiz/TempQuiz.java
+++ app/src/main/java/com/example/temperamentquiz/TempQuiz.java
@@ -2,10 +2,14 @@ package com.example.temperamentquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -17,27 +21,28 @@ import java.util.Collections;
 
 public class TempQuiz extends AppCompatActivity {
 
-    //id of each quizlet
+    //id of each question
     private int ID_CHOLERIC = R.raw.choleric;
     private int ID_MELANCHOLIC = R.raw.melancholic;
     private int ID_PHLEGMATIC = R.raw.phlegmatic;
     private int ID_SANGUINE = R.raw.sanguine;
 
-    //array of strings which stores 4 quizlet for each trait
-    private static ArrayList<String> list_choleric = new ArrayList<String>(4);
-    private static ArrayList<String> list_melancholic = new ArrayList<String>(4);
-    private static ArrayList<String> list_phlegmatic = new ArrayList<String>(4);
-    private static ArrayList<String> list_sanguine = new ArrayList<String>(4);
+    //array of strings which stores 4 question for each trait
+    private ArrayList<String> list_choleric = new ArrayList<String>(4);
+    private ArrayList<String> list_melancholic = new ArrayList<String>(4);
+    private ArrayList<String> list_phlegmatic = new ArrayList<String>(4);
+    private ArrayList<String> list_sanguine = new ArrayList<String>(4);
 
 
     //
-    private static int percent_choleric;
-    private static int percent_melancholic;
-    private static int percent_phlegmatic;
-    private static int percent_sanguine;
+    private int percent_choleric;
+    private int percent_melancholic;
+    private int percent_phlegmatic;
+    private int percent_sanguine;
 
     //
-    private static int radioWeight;
+    private int radioWeight = -1;
+    private String list_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,31 +50,108 @@ public class TempQuiz extends AppCompatActivity {
         setContentView(R.layout.activity_temp_quiz);
 
         saveFourTempQuizToArray();
+        switchQuestion();
     }
 
+    //Go to summary screen when called & save the percent values into SharedPreferences
+    public void goToSummary(View view){
+        SharedPreferences sharedPrefs = this.getSharedPreferences("temp_percent",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.clear();
+        editor.putInt("choleric",percent_choleric);
+        editor.putInt("melancholic",percent_melancholic);
+        editor.putInt("phlegmatic",percent_phlegmatic);
+        editor.putInt("sanguine",percent_sanguine);
+        editor.commit();
+//
+//        System.out.println("percent_choleric: "+percent_choleric);
+//        System.out.println("percent_melancholic: "+percent_melancholic);
+//        System.out.println("percent_phlegmatic: "+percent_phlegmatic);
+//        System.out.println("percent_sanguine: "+percent_sanguine);
+
+        Intent intent = new Intent(this,TempSummary.class);
+        startActivity(intent);
+    }
+
+    //Checks which button is clicked and add up weight
+    public void checkButtonClicked(View view){
+        if(list_id.equals("choleric") && radioWeight>-1){
+            percent_choleric+=radioWeight;
+            radioWeight = -1;
+            switchQuestion();
+        }else if(list_id.equals("melancholic") && radioWeight>-1){
+            percent_melancholic+=radioWeight;
+            radioWeight = -1;
+            switchQuestion();
+        }else if(list_id.equals("phlegmatic") && radioWeight>-1){
+            percent_phlegmatic+=radioWeight;
+            radioWeight = -1;
+            switchQuestion();
+        }else if(list_id.equals("sanguine") && radioWeight>-1){
+            percent_sanguine+=radioWeight;
+            radioWeight = -1;
+            switchQuestion();
+        }
+//        System.out.println("percent_choleric: "+percent_choleric);
+//        System.out.println("percent_melancholic: "+percent_melancholic);
+//        System.out.println("percent_phlegmatic: "+percent_phlegmatic);
+//        System.out.println("percent_sanguine: "+percent_sanguine);
+    }
+
+    //This function switches the question of the TextVIew
+    public void switchQuestion(){
+        String temp;
+
+        setContentView(R.layout.activity_temp_quiz);
+        TextView textView = (TextView) findViewById(R.id.temp_question);
+
+        if(!list_choleric.isEmpty()){
+            list_id = "choleric";
+            temp = list_choleric.remove(0);
+            textView.setText(temp);
+        }else if(!list_melancholic.isEmpty()){
+            list_id = "melancholic";
+            temp = list_melancholic.remove(0);
+            textView.setText(temp);
+        }else if(!list_phlegmatic.isEmpty()){
+            list_id = "phlegmatic";
+            temp = list_phlegmatic.remove(0);
+            textView.setText(temp);
+        }else if(!list_sanguine.isEmpty()){
+            list_id = "sanguine";
+            temp = list_sanguine.remove(0);
+            textView.setText(temp);
+        }else{
+            //TODO: GO to Result Page
+            list_id = "end";
+            goToSummary(findViewById(R.id.question_button));
+        }
+    }
+
+
+    //This function checks if the radio button was clicked and set weight
     public void checkRadioButtonClicked(View view){
         boolean checked = ((RadioButton) view).isChecked();
 
         if(checked){
-            if(view.getId() ==  R.id.radio_s_agree){
+            int id = view.getId();
+            if(id ==  R.id.radio_s_agree){
                 radioWeight = 4;
-            }else if(view.getId() == R.id.radio_p_agree){
+            }else if(id == R.id.radio_p_agree){
                 radioWeight = 3;
-            }else if(view.getId() == R.id.radio_neutral){
+            }else if(id == R.id.radio_neutral){
                 radioWeight = 2;
-            }else if(view.getId() == R.id.radio_p_disagree){
+            }else if(id == R.id.radio_p_disagree){
                 radioWeight = 1;
-            }else if(view.getId() == R.id.radio_s_disagree){
+            }else if(id == R.id.radio_s_disagree){
                 radioWeight = 0;
             }
-            System.out.println(radioWeight);
-
-
+            //System.out.println(radioWeight);
         }
 
     }
 
-    //Save four random quiz to the matching temperament list
+    //Save four random question to the matching temperament list
     private void saveFourTempQuizToArray(){
         getFourRandomQuiz(ID_CHOLERIC,list_choleric);
         getFourRandomQuiz(ID_MELANCHOLIC,list_melancholic);
@@ -78,7 +160,7 @@ public class TempQuiz extends AppCompatActivity {
     }
 
 
-    //Add four quiz to the temp_list from raw resources(id=text_id)
+    //Add four question to the temp_list from raw resources(id=text_id)
     private void getFourRandomQuiz(int text_id, ArrayList<String> temp_list){
         try{
             InputStream inputStream = getResources().openRawResource(text_id);
---
+++ app/src/main/java/com/example/temperamentquiz/TempSummary.java
@@ -0,0 +1,105 @@
+package com.example.temperamentquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class TempSummary extends AppCompatActivity {
+    private int percent_choleric;
+    private int percent_melancholic;
+    private int percent_phlegmatic;
+    private int percent_sanguine;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp_summary);
+
+        retrieveSharedPreferences();
+        String userTemp = largestTemperament();
+        if(userTemp!=null){
+            setUserTempText(userTemp);
+            setUserTempDescription(userTemp);
+            String scores = getScoresString();
+            setScore(scores);
+        }
+    }
+    private void setScore(String scores){
+        TextView textView = findViewById(R.id.temp_score);
+        textView.setText(scores);
+    }
+
+    //Generate string which shows how much score you got for each temperament
+    private String getScoresString(){
+        String score="";
+        score += "Choleric: "+percent_choleric+"\n";
+        score += "Melancholic: "+percent_melancholic+"\n";
+        score += "Phlegmatic: "+percent_phlegmatic+"\n";
+        score += "Sanguine: "+percent_sanguine;
+        return score;
+    }
+
+    //This function sets proper temperament description to the TextView
+    private void setUserTempDescription(String userTemp){
+        String description="";
+        if(userTemp.equals("choleric")){
+            description = getResources().getString(R.string.temp_choleric);
+        }else if(userTemp.equals("melancholic")){
+            description = getResources().getString(R.string.temp_melancholic);
+        }else if(userTemp.equals("phlegmatic")){
+            description = getResources().getString(R.string.temp_phlegmatic);
+        }else if(userTemp.equals("sanguine")){
+            description = getResources().getString(R.string.temp_sanguine);
+        }
+        TextView textView = findViewById(R.id.temp_description);
+        textView.setText(description);
+    }
+
+    //This function sets userTemp into TextView
+    private void setUserTempText(String userTemp){
+        TextView textView = findViewById(R.id.your_temp);
+        textView.setText(userTemp);
+    }
+
+    // This function retrieves percents of each temperament, loading from SharedPrefereneces
+    private void retrieveSharedPreferences(){
+        SharedPreferences sharedPref = this.getSharedPreferences("temp_percent",Context.MODE_PRIVATE);
+        percent_choleric = sharedPref.getInt("choleric",0);
+        percent_melancholic = sharedPref.getInt("melancholic",0);
+        percent_phlegmatic = sharedPref.getInt("phlegmatic",0);
+        percent_sanguine = sharedPref.getInt("sanguine",0);
+
+        System.out.println("percent_choleric: "+percent_choleric);
+        System.out.println("percent_melancholic: "+percent_melancholic);
+        System.out.println("percent_phlegmatic: "+percent_phlegmatic);
+        System.out.println("percent_sanguine: "+percent_sanguine);
+    }
+
+    // Returns the largest temperament feature
+    private String largestTemperament(){
+        String temp;
+
+        List<Integer> integerList = new ArrayList<Integer>(Arrays.asList(percent_choleric,percent_melancholic,percent_phlegmatic,percent_sanguine));
+        Collections.sort(integerList);
+
+        int largestNumber=integerList.get(integerList.size()-1);
+        if(largestNumber==percent_choleric){
+            return "choleric";
+        } else if(largestNumber==percent_melancholic){
+            return "melancholic";
+        } else if(largestNumber==percent_phlegmatic){
+            return "phlegmatic";
+        } else if(largestNumber==percent_sanguine){
+            return "sanguine";
+        }
+        return null;
+    }
+}
